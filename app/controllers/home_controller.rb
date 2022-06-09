806e09c14e48fb3d26c077e24ea08ac5ae8b47f5
lou <dustyfingers01257@gmail.com>
2022-06-09 16:57:33 -0600

    learned about passing vars from contollers to views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,7 @@ class HomeController < ApplicationController
     end
 
     def about
+        # this vaiable is accessible from the app/views/home/about.html.erb view
+        @about_me = "My name is Louie Williford!"
     end
 end
